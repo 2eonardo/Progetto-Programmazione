3dfaeb570fc2c53d2fb80d6492b5d8de0d4cfdd5
Leo <leonardo.soldani@edu.unifi.it>
2024-08-24 11:30:36 +0200

    Implementazione design pattern Observer

---
+++ Subject.h
@@ -0,0 +1,20 @@
+//
+// Created by Leo on 23/08/2024.
+//
+
+#ifndef PROGETTO_PROGRAMMAZIONE_SUBJECT_H
+#define PROGETTO_PROGRAMMAZIONE_SUBJECT_H
+#include "Observer.h"
+class Subject {
+public:
+    virtual void registerObserver(Observer * o)=0;
+
+    virtual void removeObserver(Observer * o)=0;
+
+    virtual void notify()=0;
+
+    virtual ~Subject() {
+
+    }
+};
+#endif //PROGETTO_PROGRAMMAZIONE_SUBJECT_H
